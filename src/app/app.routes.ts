02b7ba270a059c2cfb7f5fb487dbad651c66608f
Adalberto Gonçalves da Silva <agsilva.mhm@gmail.com>
2024-11-01 07:58:26 -0300

    Implementação do component tecnicoread

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/components/home/home.component';
+import { TecnicoReadComponent } from './views/components/tecnico/tecnico-read/tecnico-read.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: HomeComponent
-  }];
+    component: HomeComponent    
+  },
+  {
+    path:'tecnicos',
+    component: TecnicoReadComponent
+  }
+];
